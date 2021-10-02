3d8e7863ccdc14c6fc918294aaf2939f02d1ec53
Abin Gomez <40915526+cpTn3m0o@users.noreply.github.com>
2021-10-02 13:26:57 +0530

    Add files via upload

---
+++ Website1/index.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Sample Webpage</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="CSS/style.css">
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
+            <div id="navbar" class="container-fluid">
+                <a class="navbar-brand" href="index.php"><img src="images/icon1.png" class="d-block w-100" alt="icon1"></a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                      <a class="nav-link" href="index.php">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="#">Service</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+        </nav>
+        <div id="home">
+            <div>
+                <img src="images/banner1.png" class="img-fluid" alt="banner1">
+            </div>
+            <section id="heading" class="my-5">
+            <div >
+                <h1>SUSTAINABILITY</h1>
+                <h4>NO PLANET = NO PORTFOLIO|GO CARBON NEUTRAL!</h4>
+            </div>
+            </section>
+            <section class="my-5">
+                <div>
+                    <h2 class="text-center">Welcome to <span style="color: green"> Title </span> </h2>
+                </div>
+            </section>
+            <div class="row">
+                <div class="px-5 col-lg-4 col-md-4 col-12">
+                    <img src="images/Group 18.png" class="img-fluid" alt="group18">
+                    <h3><b>Bringing The Best</b></h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                </div>
+                <div class="px-5 col-lg-4 col-md-4 col-12">
+                    <img src="images/Group 26.png" class="img-fluid" alt="group18">
+                    <h3><b>Global Reach</b></h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                </div>
+                <div class="px-5 col-lg-4 col-md-4 col-12">
+                    <img src="images/Group 27.png" class="img-fluid" alt="group18">
+                    <h3><b>Global Goals Ready</b></h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                </div>
+            </div>
+            <section class="my-5">
+                <div  style="background:transparent url('images/greenBG.png') no-repeat center center /cover">
+                    <h2 class="p-5" style="color: white"> <b>Our</b> Service </h2>
+                    <p class="px-5" style="color: white">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                    <div class="row">
+                        <div class="px-5 col-lg-4 col-md-4 col-12">
+                            <img src="images/Rectangle.png" class="img-fluid" alt="group18">
+                        </div>
+                        <div class="px-5 col-lg-4 col-md-4 col-12">
+                            <img src="images/Rectangle2.png" class="img-fluid" alt="group18">
+                        </div>
+                        <div class="px-5 col-lg-4 col-md-4 col-12">
+                            <img src="images/Rectangle3.png" class="img-fluid" alt="group18">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                        <div class="px-5 col-lg-4 col-md-4 col-12">
+                            <h3 class="py-3 text-center"><b>Service Title 1</b></h3>
+                            <p class="py-3 text-center">Classify. Measure. Report Supporting uptake</p>
+                            <div class="text-center">
+                                <button type="button" class="btn btn-light border-dark" style="border-radius: 50%">Explore</button>
+                            </div>
+                        </div>
+                        <div class="px-5 col-lg-4 col-md-4 col-12">
+                            <h3 class="py-3 text-center"><b>Service Title 2</b></h3>
+                            <p class="py-3 text-center">Classify. Measure. Report Supporting uptake</p>
+                            <div class="text-center">
+                                <button type="button" class="btn btn-light border-dark" style="border-radius: 50%">Explore</button>
+                            </div>
+                        </div>
+                        <div class="px-5 col-lg-4 col-md-4 col-12">
+                            <h3 class="py-3 text-center"><b>Service Title 3</b></h3>
+                            <p class="py-3 text-center">Classify. Measure. Report Supporting uptake</p>
+                            <div class="text-center">
+                                <button type="button" class="btn btn-light border-dark" style="border-radius: 50%">Explore</button>
+                            </div>
+                        </div>
+                    </div>
+            </section>
+            
+            <section class="my-5">
+                <div  style="background:transparent url('images/blueBG.png') no-repeat center center /cover">
+                    <h2 class="p-5" style="color: white"> <b>Our</b> Service </h2>
+                    <p class="px-5" style="color: white">Lorem Ipsum is simply dummy text of the printing and typesetting industry.  Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                    <div class="row">
+                        <div class="px-5 col-lg-4 col-md-4 col-12">
+                            <img src="images/Rectangle.png" class="img-fluid" alt="group18">
+                        </div>
+                        <div class="px-5 col-lg-4 col-md-4 col-12">
+                            <img src="images/Rectangle2.png" class="img-fluid" alt="group18">
+                        </div>
+                        <div class="px-5 col-lg-4 col-md-4 col-12">
+                            <img src="images/Rectangle3.png" class="img-fluid" alt="group18">
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-5">
+                        <div class="px-5 col-lg-4 col-md-4 col-12">
+                            <h3 class="py-3 text-center"><b>Project Name</b></h3>
+                            <p class="py-3 text-center">Location</p>
+                            <div class="text-center">
+                                <button type="button" class="btn btn-light border-dark" style="border-radius: 50%">Explore</button>
+                            </div>
+                        </div>
+                        <div class="px-5 col-lg-4 col-md-4 col-12">
+                            <h3 class="py-3 text-center"><b>Project Name</b></h3>
+                            <p class="py-3 text-center">Location</p>
+                            <div class="text-center">
+                                <button type="button" class="btn btn-light border-dark" style="border-radius: 50%">Explore</button>
+                            </div>
+                        </div>
+                        <div class="px-5 col-lg-4 col-md-4 col-12">
+                            <h3 class="py-3 text-center"><b>Project Name</b></h3>
+                            <p class="py-3 text-center">Location</p>
+                            <div class="text-center">
+                                <button type="button" class="btn btn-light border-dark" style="border-radius: 50%">Explore</button>
+                            </div>
+                        </div>
+                    </div>
+            </section>
+            
+            <section class="my-5">
+                <div  style="background:transparent url('images/Path 2.png') no-repeat center center /cover">
+                    <p class="px-5 pt-5 pb-2" style="color: white">TESTIMONIALS</p>
+                    <h2 class="px-5 pb-2" style="color: white"> <b>What customers</b></h2>
+                    <h2 class="px-5" style="color: white">Say About Us.</h2>
+                    <div class="row justify-content-end">
+                        <div class="px-5 mx-1 col-lg-4 col-md-4 col-12" style="background-color: white">
+                            <h3 class="py-3 text-center"><b>Best User Experience</b></h3>
+                            <p class="py-3 text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
+                        <div class="px-5 mx-5 col-lg-4 col-md-4 col-12" style="background-color: white">
+                            <h3 class="py-3 text-center"><b>Friendly Staff</b></h3>
+                            <p class="py-3 text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            
+            <section class="my-5">
+                    <div class="row">
+                        <div class="px-5 col-lg-8 col-md-8 col-12">
+                            <h3 class="text-center">Sign Up For Our <span style="color: green"> Newsletter </span> </h3>
+                            <p class="pt-3 text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                            <p class="text-center"> Lorem Ipsum has been the industry's standard</p>
+                            <form class="form-inline justify-content-center">
+                                <div class="form-group mx-sm-3 mb-2">
+                                    <input type="email" class="form-control col-lg-12" placeholder="Enter Your Email">
+                                </div>
+                                <button type="submit" class="btn btn-primary mb-2">Subscribe</button>
+                            </form>
+                        </div>
+                        <div class="px-5 col-lg-4 col-md-4 col-12">
+                            <img src="images/userPic.png" class="img-fluid" alt="group18">
+                        </div>
+                    </div>
+            </section>
+        </div>
+        
+        <footer class="page-footer font-small blue pt-4" style="background-color: #B3DBFF">
+          <div class="container-fluid text-center text-md-left">
+            <div class="row">
+              <div class="col-md-3 mt-md-0 px-5 mt-3">
+                <a class="navbar-brand" href="index.php"><img src="images/icon1.png" class="d-block w-100" alt="icon1"></a>
+              </div>
+              <hr class="clearfix w-100 d-md-none pb-3">
+              <div class="col-md-3 mb-md-0 mb-3">
+                
+                <ul class="list-unstyled">
+                  <li>
+                    <a href="#!">Home Page</a>
+                  </li>
+                  <li>
+                    <a href="#!">About Us</a>
+                  </li>
+                  <li>
+                    <a href="#!">Careers</a>
+                  </li>
+                  <li>
+                    <a href="#!">Contact Us</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-md-3 mb-md-0 mb-3">
+                
+                <ul class="list-unstyled">
+                  <li>
+                    <h5>Service</h5>
+                  </li>
+                  <li>
+                    <a href="#!">-Service Title 1</a>
+                  </li>
+                  <li>
+                    <a href="#!">-Service Title 2</a>
+                  </li>
+                  <li>
+                    <a href="#!">-Service Title 2</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-md-3 mb-md-0 mb-3">
+                
+                <ul class="list-unstyled">
+                  <li>
+                    <a href="#!">Projects</a>
+                  </li>
+                  <li>
+                    <a href="#!">Privacy Policy</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+            <div class="row">
+                <div class="col-md-6 mt-md-0 mt-3">    
+          <div class="footer-copyright py-3 px-5 mx-5">
+              2021 © gloify
+          </div>
+                </div>
+                <div class="col-md-6 mt-md-0 mt-3"> 
+            <div class="footer-copyright py-3 px-5 mx-5 text-center">
+                <a href="#" class="fa fa-linkedin"></a>  
+                <a href="#" class="fa fa-facebook"></a>
+            </div>
+            </div>
+            </div>
+        </footer>
+
+    </body>
+</html>
